74129182506fcaba80bc00a0e80afa5af495346b
Saddab Ansari <saddabansari254@gmail.com>
2026-07-24 13:02:23 +0530

    Implement user creation and login endpoints
    Added user creation and login endpoints for authentication.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -82,3 +82,29 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]): # Had
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='Could not validate user.')
 
 # @Router Post : 1. Create User and 2. login for acess Token.
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+                      create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password)
+    )
+
+    db.add(create_user_model)
+    db.commit()
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
+
+    return {'access_token': token, 'token_type': 'bearer'}
