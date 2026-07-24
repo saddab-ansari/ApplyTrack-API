c3fc12831db1b3e98d176b573d1ac34398c2c074
Saddab Ansari <saddabansari254@gmail.com>
2026-07-24 13:01:48 +0530

    Add FastAPI setup

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# main.py will be used in the end to route all the files.
+# Main.py will be used in the end to route all the files.
+
+from fastapi import FastAPI
+import models
+from database import engine
+from routers import auth
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
