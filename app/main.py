4881fbc179ceb4ebed552d1dd2fe8d6b91a2c23d
Saddab Ansari <saddabansari254@gmail.com>
2026-07-26 14:31:09 +0530

    Add dashboard router to main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-# main.py will be used in the end to route all the files.
+# Main.py will be used in the end to route all the files.
 
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import auth,company,application, interview_round
+from routers import auth,company,application, interview_round, dashboard
 
 app = FastAPI()
 
@@ -13,3 +13,4 @@ app.include_router(auth.router)
 app.include_router(company.router)
 app.include_router(application.router)
 app.include_router(interview_round.router)
+app.include_router(dashboard.router)
